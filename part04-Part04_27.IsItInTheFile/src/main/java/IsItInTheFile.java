b03da6e5dc3a3fb8cf3f32188100110dfe642b8a
mitchellmisayah <mitchellmisayah@gmail.com>
2022-11-02 02:41:47 -0500

    IsItInTheFile Completed

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -1,10 +1,12 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class IsItInTheFile {
-
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Name of the file:");
@@ -12,6 +14,27 @@ public class IsItInTheFile {
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
+
+        boolean isFound = false;
+
+        try (Scanner scannerFile = new Scanner(Paths.get(file))) {
+            while (scannerFile.hasNextLine()) {
+                String row = scannerFile.nextLine();
+                if (row.equals(searchedFor)) {
+                    isFound = true;
+                }
+
+            }
+
+            if (isFound) {
+                System.out.println("Found!");
+            } else {
+                System.out.println("Not found.");
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            System.out.println("Reading the file " + file + " failed.");
+        }
 
     }
 }
