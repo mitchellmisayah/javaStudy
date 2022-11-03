180f07e0cb538d4fb37c57480dbdbba16eef2fee
mitchellmisayah <mitchellmisayah@gmail.com>
2022-11-03 04:59:14 -0500

    RecordsFromAFile Done

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -1,40 +1,51 @@
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RecordsFromAFile {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        // 1. Create a scanner for user input.
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Name of the file:");
+        // 2. Prompt the user for the name of the file.
+        System.out.println("Name of the file: ");
 
+        // 3. Store the user input into a String variable.
         String file = scanner.nextLine();
 
-        try ( Scanner fileScanner = new Scanner(Paths.get(file))) {
+        // Data will look like this: lily,3.
+        // You want the out put to look like this: lily, age: 3 years.
+
+        String[] dataArray;
+        String name, row;
+        int age;
+
+        // 4. Implement reading file.
+        try (Scanner fileScanner = new Scanner(Paths.get(file))) {
+            // we read the file until all lines have been read.
 
             while (fileScanner.hasNextLine()) {
+                row = fileScanner.nextLine();
+                // we read one line.
+                dataArray = row.split(",");
+                name = dataArray[0];
+                age = Integer.valueOf(dataArray[1]);
 
-                String[] splitString = fileScanner.nextLine().split(",");
-
-                if (splitString.equals("")) {
-                    continue;
-                }
-                String name = splitString[0];
-
-                int age = Integer.valueOf(splitString[1]);
-
-                if (age > 1 || age == 0) {
-                    System.out.println(name + ", age: " + age + " years");   //if they are older than 1 or 0 USES YEARS
+                if (age == 1) {
+                    System.out.println(name + ", " + "age: " + age + " year");
                 } else {
-                    System.out.println(name + ", age: " + age + " year");
+                    System.out.println(name + ", " + "age: " + age + " years");
                 }
 
             }
 
         } catch (Exception e) {
-
-            System.out.println("File was not able to be read" + e.getMessage());
+            // TODO: handle exception
+            System.out.println("Error: " + e.getMessage());
         }
 
     }
+
 }
