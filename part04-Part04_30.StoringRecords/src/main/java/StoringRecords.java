ad9d79c4adc2ad388f6d5d8b717f3e33c4777374
mitchellmisayah <mitchellmisayah@gmail.com>
2022-11-03 05:51:18 -0500

    SportStatistics Done

--- part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
+++ part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
@@ -22,9 +22,29 @@ public class StoringRecords {
 
     public static ArrayList<Person> readRecordsFromFile(String file) {
         ArrayList<Person> persons = new ArrayList<>();
+        String[] dataArray;
+        String row, name;
+        int age;
 
         // Write here the code for reading from file
         // and printing the read records
+
+        try (Scanner scanner = new Scanner(Paths.get(file))) {
+            // we read the file until all lines have been read.
+            while (scanner.hasNextLine()) {
+                row = scanner.nextLine();
+                dataArray = row.split(",");
+
+                name = dataArray[0];
+                age = Integer.valueOf(dataArray[1]);
+
+                persons.add(new Person(name, age));
+
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Error: " + e.getMessage());
+        }
         return persons;
 
     }
--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -6,7 +6,61 @@ import java.util.Scanner;
 public class SportStatistics {
 
     public static void main(String[] args) {
+        String[] splitString;
+
+        int gameCounter = 0;
+        int winCounter = 0;
+        int loseCounter = 0;
+
         Scanner scan = new Scanner(System.in);
+
+        // Prompt user for file name.
+        System.out.println("File: ");
+        String file = scan.nextLine();
+
+        // Prompt user for team name.
+        System.out.println("Team: ");
+        String team = scan.nextLine().toLowerCase();
+
+        try (Scanner scanner = new Scanner(Paths.get(file))) {
+            // we read the file until all lines have been read.
+            while (scanner.hasNextLine()) {
+
+                // we read one line.
+                String row = scanner.nextLine();
+                splitString = row.split(",");
+                String homeTeam = splitString[0].trim();
+                String awayTeam = splitString[1].trim();
+
+                int homePoints = Integer.valueOf(splitString[2].trim());
+                int awayPoints = Integer.valueOf(splitString[3].trim());
+
+                if (homeTeam.toLowerCase().equals(team)) {
+                    gameCounter++;
+                    if (homePoints > awayPoints) {
+                        winCounter++;
+                    } else {
+                        loseCounter++;
+                    }
+                }
+
+                if (awayTeam.toLowerCase().equals(team)) {
+                    gameCounter++;
+                    if (awayPoints > homePoints) {
+                        winCounter++;
+                    } else {
+                        loseCounter++;
+                    }
+                }
+
+            }
+            System.out.println("Games: " + gameCounter);
+            System.out.println("Wins: " + winCounter);
+            System.out.println("Losses: " + loseCounter);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
 
