47646a874891a474d3334b6095adc817812075ec
mitchellmisayah <mitchellmisayah@gmail.com>
2022-11-08 04:38:32 -0600

    commit

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -3,12 +3,52 @@ public class Apartment {
 
     private int rooms;
     private int squares;
-    private int princePerSquare;
+    private int pricePerSquare;
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
-        this.princePerSquare = pricePerSquare;
+        this.pricePerSquare = pricePerSquare;
+    }
+
+    public boolean largerThan(Apartment compared) {
+
+        // first compare rooms.
+
+        if (this.rooms > compared.rooms) {
+            return true;
+        }
+        if (this.rooms < compared.rooms) {
+            return false;
+        }
+        // second compare square.
+
+        if (this.squares > compared.squares) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public int priceDifference(Apartment compared) {
+        int thisApartmentPrice = this.pricePerSquare * this.squares;
+        int comparedApartmentPrice = compared.pricePerSquare * compared.squares;
+
+        if (thisApartmentPrice > comparedApartmentPrice) {
+            return thisApartmentPrice - comparedApartmentPrice;
+        } else {
+            return comparedApartmentPrice - thisApartmentPrice;
+        }
+    }
+
+    public boolean moreExpensiveThan(Apartment compared) {
+        int thisApartmentPrice = this.pricePerSquare * this.squares;
+        int comparedApartmentPrice = compared.pricePerSquare * compared.squares;
+
+        if (thisApartmentPrice > comparedApartmentPrice) {
+            return true;
+        }
+        return false;
     }
 
 }
