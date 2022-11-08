55241d338952781eabb29256367e94583931a054
mitchellmisayah <mitchellmisayah@gmail.com>
2022-11-08 05:22:18 -0600

    commit

--- part05-Part05_13.IdenticalTwins/src/main/java/Person.java
+++ part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -1,6 +1,3 @@
-
-import java.util.Objects;
-
 public class Person {
 
     private String name;
@@ -16,4 +13,35 @@ public class Person {
     }
 
     // implement an equals method here for checking the equality of objects
+    public boolean equals(Object compared) {
+        // if the variables are located in the same place, they are the same.
+        if (this == compared) {
+            return true;
+        }
+
+        // if the object is not instance of person, the objects are not the same
+        if (!(compared instanceof Person)) {
+            return false;
+        }
+
+        // let's cast the object to SimpleDate
+        Person otherPerson = (Person) compared;
+
+        // if the object's values are the same, the objects are the same
+        if (this.name.equals(otherPerson.name)
+
+                && this.birthday.equals(otherPerson.birthday)
+
+                && this.height == otherPerson.height
+
+                && this.weight == otherPerson.weight)
+
+        {
+            return true;
+        }
+
+        // other wise, the objects are not the same
+        return false;
+    }
+
 }
