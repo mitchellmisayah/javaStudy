09475cf917ef278e97a67d6080f87d433147822e
mitchellmisayah <mitchellmisayah@gmail.com>
2022-11-10 08:53:02 -0600

    done

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -1,4 +1,3 @@
-
 public class Money {
 
     private final int euros;
@@ -23,6 +22,52 @@ public class Money {
         return this.cents;
     }
 
+    public Money plus(Money addition) {
+
+        int newEuros = euros + addition.euros();
+        int newCents = cents + addition.cents();
+        if (newCents > 99) {
+            newCents = newCents - 100;
+            newEuros = newEuros + 1;
+        }
+
+        return new Money(newEuros, newCents);
+    }
+
+    public boolean lessThan(Money compared) {
+
+        boolean isLessThan = false;
+
+        if ((100 * euros + cents) < (100 * compared.euros() + compared.cents())) {
+
+            isLessThan = true;
+        }
+
+        return isLessThan;
+    }
+
+    public Money minus(Money decreaser) {
+
+        int newEuros = euros - decreaser.euros();
+        int newCents = cents - decreaser.cents();
+        if (newCents < 0) {
+            newCents = newCents + 100;
+            newEuros = newEuros - 1;
+        }
+        // if the value becomes negative, return zero
+        if (newEuros < 0) {
+            newEuros = 0;
+            newCents = 0;
+
+        }
+
+        Money newMoney = new Money(newEuros, newCents);
+
+        return newMoney;
+
+    }
+
+    @Override
     public String toString() {
         String zero = "";
         if (this.cents < 10) {
